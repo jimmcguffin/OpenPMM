6bb78411e01e27ee18355941dbcd7ea93d6ee01a
Jim McGuffin <kw6w@arrl.net>
2025-12-12 11:49:50 -0800

    Inital commit

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -1,5 +1,22 @@
 # pylint:  disable="line-too-long,missing-function-docstring,multiple-statements,no-name-in-module"
+"""
+    OpenPMM - Packet Message Manager
+    Copyright (C) 2025  Jim McGuffin
 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
 from enum import Enum
 from operator import attrgetter
 import sys
